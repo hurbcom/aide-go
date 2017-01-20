bc63fd4adde16a3092a9255ce6e23b84dd03d901
Bernardo Loureiro <bernardo.lou@gmail.com>
2017-01-20 15:54:03 -0200

    Adding reminder to document

--- lib/main.go
+++ lib/main.go
@@ -49,6 +49,7 @@ const (
 	datePatternYYYYMMDDTHHMMSSZ = "2006-01-02T15:04:05Z"
 )
 
+// ToStringSlice REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ToStringSlice(intslice []int) (stringSlice []string) {
 	for _, i := range intslice {
 		stringSlice = append(stringSlice, strconv.FormatInt(int64(i), 10))
@@ -56,6 +57,7 @@ func ToStringSlice(intslice []int) (stringSlice []string) {
 	return stringSlice
 }
 
+// ToStringSlice64 REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ToStringSlice64(int64Slice []int64) (stringSlice []string) {
 	for _, i := range int64Slice {
 		stringSlice = append(stringSlice, strconv.FormatInt(i, 10))
@@ -63,6 +65,7 @@ func ToStringSlice64(int64Slice []int64) (stringSlice []string) {
 	return stringSlice
 }
 
+// ToInt64Slice REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ToInt64Slice(stringSlice []string) (int64Slice []int64) {
 	for _, i := range stringSlice {
 		intI, err := ParseStringToInt64(i)
@@ -74,6 +77,7 @@ func ToInt64Slice(stringSlice []string) (int64Slice []int64) {
 	return int64Slice
 }
 
+// ParseStringToInt REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ParseStringToInt(s string) (int, error) {
 	if s == "" {
 		return 0, nil
@@ -82,6 +86,7 @@ func ParseStringToInt(s string) (int, error) {
 	return strconv.Atoi(s)
 }
 
+// ParseStringToInt64 REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ParseStringToInt64(s string) (int64, error) {
 	if s == "" {
 		return 0, nil
@@ -90,10 +95,12 @@ func ParseStringToInt64(s string) (int64, error) {
 	return strconv.ParseInt(s, 10, 0)
 }
 
+// ParseDateYearMonthDay REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ParseDateYearMonthDay(dateString string) (time.Time, error) {
 	return time.Parse(datePatternYYYYMMDD, dateString)
 }
 
+// DiffDays REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func DiffDays(date1 time.Time, date2 time.Time) (int64, error) {
 	if !date1.IsZero() && !date2.IsZero() {
 		duration := date2.Sub(date1)
@@ -102,6 +109,7 @@ func DiffDays(date1 time.Time, date2 time.Time) (int64, error) {
 	return 0, errors.New("invalid-dates")
 }
 
+// ParseDateStringToTime REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ParseDateStringToTime(dateString string) (*time.Time, error) {
 	var result time.Time
 	var err error
@@ -127,6 +135,7 @@ func ParseDateStringToTime(dateString string) (*time.Time, error) {
 	return &result, err
 }
 
+// RemoveNanoseconds REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func RemoveNanoseconds(date time.Time) (time.Time, error) {
 	dateWithoutNSecs, err := ParseDateStringToTime(date.Format(time.RFC3339))
 	if err != nil {
@@ -135,6 +144,7 @@ func RemoveNanoseconds(date time.Time) (time.Time, error) {
 	return *dateWithoutNSecs, nil
 }
 
+// ParseIntToBool REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ParseIntToBool(i int) bool {
 	if i == 1 {
 		return true
@@ -142,6 +152,7 @@ func ParseIntToBool(i int) bool {
 	return false
 }
 
+// ParseStringToBool REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ParseStringToBool(s string) bool {
 	if s == "1" {
 		return true
@@ -149,6 +160,7 @@ func ParseStringToBool(s string) bool {
 	return false
 }
 
+// ParseBoolToString REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ParseBoolToString(b bool) string {
 	if b {
 		return "1"
@@ -156,6 +168,7 @@ func ParseBoolToString(b bool) string {
 	return "0"
 }
 
+// CheckStringJsonData REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func CheckStringJsonData(s string) *string {
 	if len(s) > 0 {
 		return &s
@@ -163,6 +176,7 @@ func CheckStringJsonData(s string) *string {
 	return nil
 }
 
+// CheckInt64JsonData REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func CheckInt64JsonData(i int64) *int64 {
 	if i > 0 {
 		return &i
@@ -170,6 +184,7 @@ func CheckInt64JsonData(i int64) *int64 {
 	return nil
 }
 
+// CheckFloat64JsonData REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func CheckFloat64JsonData(f float64) *float64 {
 	if f > 0 {
 		return &f
@@ -177,6 +192,7 @@ func CheckFloat64JsonData(f float64) *float64 {
 	return nil
 }
 
+// GetByteArrayAndBufferFromRequestBody REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func GetByteArrayAndBufferFromRequestBody(body io.ReadCloser) ([]byte, *bytes.Buffer, error) {
 	defer body.Close()
 	byteArray, err := ioutil.ReadAll(body)
@@ -187,10 +203,12 @@ func GetByteArrayAndBufferFromRequestBody(body io.ReadCloser) ([]byte, *bytes.Bu
 	return byteArray, buffer, nil
 }
 
+// GetOnlyNumbers REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func GetOnlyNumbers(s *string) *string {
 	return GetOnlyNumbersOrSpecial(s, "")
 }
 
+// GetOnlyNumbersOrSpecial REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func GetOnlyNumbersOrSpecial(s *string, sp string) *string {
 	if s == nil {
 		return s
@@ -208,6 +226,7 @@ func GetOnlyNumbersOrSpecial(s *string, sp string) *string {
 	}
 }
 
+// GetStringBodyHttpRequest REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func GetStringBodyHttpRequest(r *http.Request) *string {
 	if r == nil {
 		return nil
@@ -230,6 +249,7 @@ func GetStringBodyHttpRequest(r *http.Request) *string {
 	return nil
 }
 
+// GetStringBodyHttpRequestJSON REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func GetStringBodyHttpRequestJSON(r *http.Request) *string {
 	result := GetStringBodyHttpRequest(r)
 	if result != nil {
@@ -243,6 +263,7 @@ func GetStringBodyHttpRequestJSON(r *http.Request) *string {
 	return nil
 }
 
+// GetStringBodyHttpResponse REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func GetStringBodyHttpResponse(r *http.Response) *string {
 	if r == nil {
 		return nil
@@ -265,6 +286,7 @@ func GetStringBodyHttpResponse(r *http.Response) *string {
 	return nil
 }
 
+// GetStringBodyHttpResponseJSON REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func GetStringBodyHttpResponseJSON(r *http.Response) *string {
 	result := GetStringBodyHttpResponse(r)
 	if result != nil {
@@ -278,6 +300,7 @@ func GetStringBodyHttpResponseJSON(r *http.Response) *string {
 	return nil
 }
 
+// ParseIntOrReturnZero REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func ParseIntOrReturnZero(s string) int {
 	integer, err := strconv.ParseInt(s, 10, 0)
 	if err != nil {
@@ -290,6 +313,7 @@ type Stringer interface {
 	String() string
 }
 
+// IsArray REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func IsArray(arg interface{}) bool {
 	if arg == nil {
 		return false
@@ -297,6 +321,7 @@ func IsArray(arg interface{}) bool {
 	return reflect.TypeOf(arg).Kind() == reflect.Slice
 }
 
+// IsString REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func IsString(arg interface{}) bool {
 	if arg == nil {
 		return false
@@ -304,6 +329,7 @@ func IsString(arg interface{}) bool {
 	return reflect.TypeOf(arg).Kind() == reflect.String
 }
 
+// IsPointer REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func IsPointer(arg interface{}) bool {
 	if arg == nil {
 		return false
@@ -311,6 +337,7 @@ func IsPointer(arg interface{}) bool {
 	return reflect.TypeOf(arg).Kind() == reflect.Ptr
 }
 
+// Join REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func Join(sep string, args ...interface{}) string {
 	var buf bytes.Buffer
 	var str Stringer
@@ -356,6 +383,7 @@ func Join(sep string, args ...interface{}) string {
 	return buf.String()
 }
 
+// BeginningOfToday REQUIRE THEM TO DOCUMENT THIS FUNCTION
 func BeginningOfToday() time.Time {
 	now := time.Now()
 	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
