ac08dea821f795f3e722daf51859bbe0e43970e1
Bernardo Loureiro <bernardo.lou@gmail.com>
2018-04-11 13:34:34 -0300

    Using dummy main command

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/hotelurbano/aide-go/lib"
 )
 
 func main() {
-	fmt.Printf("Testing build at %s", lib.BeginningOfToday())
+	fmt.Println("Hello world")
 }
