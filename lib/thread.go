30ea67ff3f705a6602d7599c402fe3b0bd1ee86e
Luis F F Spaniol <luis.spaniol@hurb.com>
2021-03-03 18:51:41 -0300

    add a waring

--- lib/thread.go
+++ lib/thread.go
@@ -7,6 +7,7 @@ import (
 )
 
 // ErrorGroup resolves all go routines. It fails at first error encountered
+// warning: Creating goroutines in the args will cause them to run in background
 func ErrorGroup(ctx context.Context, args ...func() error) error {
 	g, _ := errgroup.WithContext(ctx)
 
@@ -17,5 +18,6 @@ func ErrorGroup(ctx context.Context, args ...func() error) error {
 	if err := g.Wait(); err != nil {
 		return err
 	}
+
 	return nil
 }
