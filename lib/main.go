3eee755fe4115d7a7880ae2b1edf2336884d1b44
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-05-17 20:01:37 -0300

    Adding check errors

--- lib/main.go
+++ lib/main.go
@@ -303,8 +303,17 @@ func GetStringBodyHTTPRequest(r *http.Request) *string {
 	if r == nil {
 		return nil
 	}
-	headers, _ := httputil.DumpRequest(r, false)
-	headersAndBody, _ := httputil.DumpRequest(r, true)
+
+	headers, err := httputil.DumpRequest(r, false)
+	if err != nil {
+		return nil
+	}
+
+	headersAndBody, err := httputil.DumpRequest(r, true)
+	if err != nil || len(headersAndBody) == 0 {
+		return nil
+	}
+
 	body := headersAndBody[len(headers):]
 	s := string(bytes.TrimSpace(body))
 	return &s
@@ -315,15 +324,26 @@ func GetStringBodyHTTPRequestJSON(r *http.Request) *string {
 	if r == nil {
 		return nil
 	}
-	headers, _ := httputil.DumpRequest(r, false)
-	headersAndBody, _ := httputil.DumpRequest(r, true)
+
+	headers, err := httputil.DumpRequest(r, false)
+	if err != nil {
+		return nil
+	}
+
+	headersAndBody, err := httputil.DumpRequest(r, true)
+	if err != nil || len(headersAndBody) == 0 {
+		return nil
+	}
+
 	body := bytes.TrimSpace(headersAndBody[len(headers):])
+
 	if len(body) > 0 {
 		start := bytes.IndexAny(body, "{")
 		end := bytes.LastIndexAny(body, "}")
 		r := string(body[start : end+1])
 		return &r
 	}
+
 	return nil
 }
 
@@ -332,8 +352,17 @@ func GetStringBodyHTTPResponse(r *http.Response) *string {
 	if r == nil {
 		return nil
 	}
-	headers, _ := httputil.DumpResponse(r, false)
-	headersAndBody, _ := httputil.DumpResponse(r, true)
+
+	headers, err := httputil.DumpResponse(r, false)
+	if err != nil {
+		return nil
+	}
+
+	headersAndBody, err := httputil.DumpResponse(r, true)
+	if err != nil || len(headersAndBody) == 0 {
+		return nil
+	}
+
 	body := headersAndBody[len(headers):]
 	s := string(bytes.TrimSpace(body))
 	return &s
@@ -344,8 +373,17 @@ func GetStringBodyHTTPResponseJSON(r *http.Response) *string {
 	if r == nil {
 		return nil
 	}
-	headers, _ := httputil.DumpResponse(r, false)
-	headersAndBody, _ := httputil.DumpResponse(r, true)
+
+	headers, err := httputil.DumpResponse(r, false)
+	if err != nil {
+		return nil
+	}
+
+	headersAndBody, err := httputil.DumpResponse(r, true)
+	if err != nil || len(headersAndBody) == 0 {
+		return nil
+	}
+
 	body := bytes.TrimSpace(headersAndBody[len(headers):])
 	if len(body) > 0 {
 		start := bytes.IndexAny(body, "{")
